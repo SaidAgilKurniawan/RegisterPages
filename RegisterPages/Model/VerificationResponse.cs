f77f928b99b9ded9f917c15f8d364ad0da4cea85
SaidAgilKurniawan <92496571+SaidAgilKurniawan@users.noreply.github.com>
2021-12-02 08:45:03 +0700

    Add files via upload

---
+++ RegisterPages/Model/VerificationResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegisterPages.Model
+{
+    interface VerificationResponse
+    {
+         void OnWaVerificationSucceed(object source, EventArgs e);
+         void OnEmailVerificationSucceed(object source, EventArgs e);
+    }
+}
